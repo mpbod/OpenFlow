b99e244535aa35ec4e87a9d3c22c7454ad614618
Holtwick <dirk.holtwick@gmail.com>
2009-08-19 09:12:52 +0200

    setup small demo showing a smaller openflow view

---
+++ SmallFlowDemo/Classes/XBase.h
@@ -0,0 +1,19 @@
+/*
+ *  XBase.h
+ *
+ *  Little helper for the 'xobjc' convienience tool:
+ *  http://github.com/holtwick/xobjc/tree
+ *
+ *  Created by Dirk on 12.08.09.
+ *  Copyright 2009 holtwick.it. All rights reserved.
+ *
+ */
+
+#import <UIKit/UIKit.h>
+
+#define XASSIGN
+#define XRETAIN 
+#define XCOPY 
+#define XATOMIC 
+#define XREADONLY
+#define XIBOUTLET
